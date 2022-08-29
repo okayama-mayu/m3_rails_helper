db6a7c51e1e7891e9f283bfaa7070163d38ca781
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-08-29 19:10:18 -0400

    Add feature to get all Merchants

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -7,5 +7,19 @@ describe 'Merchants API' do
     get '/api/v1/merchants' 
 
     expect(response).to be_successful 
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchants).to be_an Array 
+
+    expect(merchants.count).to eq 3 
+
+    merchants.each do |merchant| 
+      expect(merchant).to have_key(:id) 
+      expect(merchant[:id]).to be_an Integer 
+
+      expect(merchant).to have_key(:name)
+      expect(merchant[:name]).to be_a String 
+    end
   end
 end
