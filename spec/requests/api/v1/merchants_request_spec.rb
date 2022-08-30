a6a795aae5ec761f19a113970ac53b61ba644149
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-08-30 15:31:05 -0400

    Add MerchantSerializer

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -10,16 +10,17 @@ describe 'Merchants API' do
 
     merchants = JSON.parse(response.body, symbolize_names: true)
 
-    expect(merchants).to be_an Array 
+    expect(merchants).to be_a Hash
+    expect(merchants[:data]).to be_an Array 
 
-    expect(merchants.count).to eq 3 
+    expect(merchants[:data].count).to eq 3 
 
-    merchants.each do |merchant| 
+    merchants[:data].each do |merchant| 
       expect(merchant).to have_key(:id) 
-      expect(merchant[:id]).to be_an Integer 
+      expect(merchant[:id]).to be_a String  
 
-      expect(merchant).to have_key(:name)
-      expect(merchant[:name]).to be_a String 
+      expect(merchant[:attributes]).to have_key(:name)
+      expect(merchant[:attributes][:name]).to be_a String 
     end
   end
 
