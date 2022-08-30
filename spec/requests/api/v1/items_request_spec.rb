22f667e3a5dd50191a1adec33667c14531f3a6c0
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-08-30 18:18:57 -0400

    Add test for Item edit

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -107,4 +107,18 @@ RSpec.describe 'Items API' do
 
     expect(response).to have_http_status(404)
   end
+
+  it 'edits an Item' do 
+    id = create(:item).id 
+    previous_name = Item.last.name 
+    item_params = { name: 'iPhone 20' }
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    patch "/api/v1/items/#{id}", headers: headers, params: JSON.generate({item: item_params})
+    item = Item.find_by(id: id)
+
+    expect(response).to be_successful
+    expect(item.name).to_not eq(previous_name)
+    expect(item.name).to eq(item_params[:name])
+  end
 end
