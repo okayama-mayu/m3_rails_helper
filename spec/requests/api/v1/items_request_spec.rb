b3019dca7d7d93dc2ae5ea1f4cea0c53890b3927
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-08-30 19:05:14 -0400

    Add test for delete Item

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -132,4 +132,20 @@ RSpec.describe 'Items API' do
 
     expect(response).to have_http_status(404)
   end
+
+  it 'deletes an Item' do 
+    create_list(:merchant, 4)
+    item = create(:item)
+
+    expect(Item.count).to eq(1) 
+    expect(Merchant.count).to eq(4)
+
+    delete "/api/v1/items/#{item.id}"
+
+    expect(response).to have_http_status(204)
+    expect(response).to be_successful
+    expect(Item.count).to eq 0 
+    expect(Merchant.count).to eq 3 
+    expect{Item.find(item.id)}.to raise_error(ActiveRecord::RecordNotFound)
+  end
 end
