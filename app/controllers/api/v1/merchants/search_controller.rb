ea0b682bdac5b90c7d5f34eeedb4f4dcaad84532
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-08-31 13:53:41 -0400

    Edit sad path test for Merchant search

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::Merchants::SearchController < ApplicationController
   def show 
     merchant = Merchant.all.find_merchant(params[:name])
-    response = merchant_json_response(merchant)
+    merchant_json_response(merchant)
   end
 end
