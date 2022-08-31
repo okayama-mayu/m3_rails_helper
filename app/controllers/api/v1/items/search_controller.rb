d75b0b4be23c0ea2960597aa5e532e72d8c930bb
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-08-31 18:40:47 -0400

    Add feature to query Items by min price

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -2,7 +2,10 @@ class Api::V1::Items::SearchController < ApplicationController
   def index 
     if params[:name]
       items = Item.find_name(params[:name])
-      test = item_json_response(items)
+      item_json_response(items)
+    elsif params[:min_price]
+      items = Item.min_price(params[:min_price])
+      item_json_response(items)
     end
   end
 end
