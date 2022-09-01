4c840ff161adeda4338fc1530a39a4a74f9c409e
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-09-01 13:48:07 -0400

    Add Invoice model test for #check_dependent_destroy

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,4 +1,8 @@
 class Invoice < ApplicationRecord
   has_many :invoice_items
   has_many :items, through: :invoice_items
+
+  def check_dependent_destroy
+    
+  end
 end
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -5,4 +5,21 @@ RSpec.describe Invoice, type: :model do
     it { should have_many :invoice_items }
     it { should have_many(:items).through(:invoice_items) }
   end
+
+  describe 'instance method' do 
+    it 'deletes the Invoice if it has no Items' do 
+      customer = Customer.create!(first_name: 'Carlos', last_name: 'Stich')
+      merchant = create(:merchant)
+      invoice = Invoice.create!(customer_id: customer.id, merchant_id: merchant.id, status: 'pending')
+
+      invoice.check_dependent_destroy 
+
+      expect(Invoice.count).to eq 0 
+      expect{invoice.find(invoice.id)}.to raise_error(ActiveRecord::RecordNotFound)
+    end
+
+    xit 'does not delete the Invoice if it has an Item' do 
+
+    end
+  end
 end
