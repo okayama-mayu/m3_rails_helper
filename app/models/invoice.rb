580138595aa5b9a9b8a0751d22d93b6650ae099e
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-09-01 13:56:27 -0400

    Add relationship between Invoice and Customer in model

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,8 +1,9 @@
 class Invoice < ApplicationRecord
   has_many :invoice_items
   has_many :items, through: :invoice_items
+  belongs_to :customer 
 
   def check_dependent_destroy
-    
+
   end
 end
