9b1e0d76099d23b8e6c272375aa58653a54f0111
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-08-31 21:39:11 -0400

    Add params_check helper method to Item Search Controller

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,16 +1,21 @@
 class Api::V1::Items::SearchController < ApplicationController
   def index 
-    if (params.has_key?(:name)) && ((params.has_key?(:min_price)) || (params.has_key?(:max_price)))
-      raise ActiveRecord::RecordInvalid.new
-    elsif params[:name]
-      items = Item.find_name(params[:name])
-    elsif params[:min_price] && params[:max_price]
-      items = Item.min_max(params[:min_price], params[:max_price])
-    elsif params[:min_price]
-      items = Item.min_price(params[:min_price])
-    elsif params[:max_price]
-      items = Item.max_price(params[:max_price])
-    end
-    item_json_response(items)
+    params_check
+    item_json_response(@items)
   end
+
+  private 
+    def params_check 
+      if (params.has_key?(:name)) && ((params.has_key?(:min_price)) || (params.has_key?(:max_price)))
+        raise ActiveRecord::RecordInvalid.new
+      elsif params[:name]
+        @items = Item.find_name(params[:name])
+      elsif params[:min_price] && params[:max_price]
+        @items = Item.min_max(params[:min_price], params[:max_price])
+      elsif params[:min_price]
+        @items = Item.min_price(params[:min_price])
+      elsif params[:max_price]
+        @items = Item.max_price(params[:max_price])
+      end
+    end
 end
