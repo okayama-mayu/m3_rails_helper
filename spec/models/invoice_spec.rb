07f7ae1f3e7a2fe9e83dd37035d1f6d251fdf4ea
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-09-01 14:30:59 -0400

    Add sad path to not delete Invoice if it has an Item

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Invoice, type: :model do
   end
 
   describe 'instance method' do 
-    it 'deletes the Invoice if it has no Items' do 
+    it 'deletes the Invoice and associated Invoice Items if it has no Items' do 
       customer = Customer.create!(first_name: 'Carlos', last_name: 'Stich')
       merchant = create(:merchant)
       invoice = Invoice.create!(customer_id: customer.id, merchant_id: merchant.id, status: 'pending')
@@ -24,8 +24,18 @@ RSpec.describe Invoice, type: :model do
       expect{InvoiceItem.find(invoice_item.id)}.to raise_error(ActiveRecord::RecordNotFound)
     end
 
-    xit 'does not delete the Invoice if it has an Item' do 
+    it 'does not delete the Invoice if it has an Item' do 
+      customer = Customer.create!(first_name: 'Carlos', last_name: 'Stich')
+      merchant = create(:merchant)
+      invoice = Invoice.create!(customer_id: customer.id, merchant_id: merchant.id, status: 'pending')
+      item = merchant.items.create!(name: 'phone', description: 'abc', unit_price: 5.0)
+      invoice_item = InvoiceItem.create!(item_id: item.id, invoice_id: invoice.id, quantity: 2, unit_price: 5.0)
 
+      invoice.check_dependent_destroy 
+
+      expect(Invoice.count).to eq 1
+      expect(Invoice.find(invoice.id)).to eq invoice 
+      expect(InvoiceItem.find(invoice_item.id)).to eq invoice_item
     end
   end
 end
